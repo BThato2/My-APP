6988bb5ed6e8f12b3a657bed6b783ec02f5d7c5b
Thato <bthato@b0be836bf792.ant.amazon.com>
2023-04-11 18:11:57 +0200

    Firstb commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/bthato/Documents/My-APP/src/app/page": [
+      "/Users/bthato/Documents/My-APP/src/app/page.module.css",
+      "/Users/bthato/Documents/My-APP/node_modules/next/font/google/target.css?{\"path\":\"src/app/page.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/bthato/Documents/My-APP/src/app/globals.css"
+    ]
+  },
+  "/Users/bthato/Documents/My-APP/src/app/page.js": [
+    "/Users/bthato/Documents/My-APP/src/app/page.module.css",
+    "/Users/bthato/Documents/My-APP/node_modules/next/font/google/target.css?{\"path\":\"src/app/page.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+  ],
+  "/Users/bthato/Documents/My-APP/src/app/layout.js": [
+    "/Users/bthato/Documents/My-APP/src/app/globals.css"
+  ]
+}
